bb28e40c2755d819319bae002f8e42861fc671d1
Cory Green <corz13@hotmail.co.uk>
2022-02-25 12:57:37 +0000

    Initial Commit

---
+++ platform-stats-worker/API/RainbowSixAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace platform_stats_worker.API
+{
+    internal class RainbowSixAPI
+    {
+        public async Task<bool> GetPlayerStats()
+        {
+            Console.WriteLine($"Fetching stats for Rainbow Six Siege for {0} accounts...");
+            Thread.Sleep(5000);
+            throw new NotImplementedException("API Request not yet implemented.");
+        }
+    }
+}
---
+++ platform-stats-worker/Program.cs
@@ -0,0 +1,25 @@
+﻿using platform_stats_worker.API;
+
+Console.WriteLine("Stats worker started...");
+while (await StartStatsWorkerAsync()) {
+    Console.WriteLine($"Fetching stats for services: {"Test"}");
+    Thread.Sleep(2000);
+}
+
+Console.WriteLine("Stats worker stopped...");
+throw new Exception("Stats worker has encountered an error, please review logs.");
+
+static async Task<bool> StartStatsWorkerAsync()
+{
+    try
+    {
+        RainbowSixAPI RS6Api = new();
+        await RS6Api.GetPlayerStats();
+        return true;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+        return false;
+    }
+} 
